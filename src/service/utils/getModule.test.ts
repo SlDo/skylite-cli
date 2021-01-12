441502625e7d7bdc1b8578d6af83594fb7bac03e
SlDo <slavadodonov19@gmail.com>
2021-01-13 01:48:40 +0300

    fix tests

--- src/service/utils/getModule.test.ts
+++ src/service/utils/getModule.test.ts
@@ -4,6 +4,7 @@ import getModulePath from './getModule';
 describe('getModulePath', () => {
   test('it should return the path to module', async () => {
     const expressTemplate = path.join(__dirname, '../generator', 'templates/express/modules/dal.js');
+    console.log(expressTemplate);
     expect(getModulePath('express', 'dal.js')).toBe(expressTemplate);
   });
 });
